769c859cf865eab003d0760b0ae5027f40e9298a
guilhermedsm16 <00001114798769sp@al.educacao.sp.gov.br>
2024-11-19 16:06:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Genética',
+    'Quais são as bases nitrogenadas presentes no DNA?',
+    'Citocina, Guanina, Tinina e Adenina.'
+)
+
+criaCartao(
+    'Citologia',
+    'Qual a função da organela Mitocôndria?',
+    'A função dela é a respiração celular.'
+)
+
+criaCartao(
+    'Ecologia',
+    'Por que as plantas são chamadas de seres autótrafos?',
+    'Porque elas são capazes de produzir seu próprio alimento.'
+)
+
+criaCartao(
+    'Radiação',
+    'Por que a radiação é prejudicial para a saúde humana?',
+    'Pois ela pode causar mutações genéticas que podem ocasionar a geração de tumores.'
+)
+
+criaCartao(
+    'Ciclo biogeoquímicos',
+    'Qual o principal perigo de expor enxofre na atmosfera?',
+    'Ampliação do efeito da chuva ácida.'
+)
